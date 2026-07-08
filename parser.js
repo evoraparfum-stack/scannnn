3fa14157e0e86db6a1e12384e545beeea396583a
evoraparfum-stack <farhansssyah@gmail.com>
2026-07-09 00:04:44 +0700

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,5 +1,42 @@
 function parseOCR(text){
 
-return text;
+text = text.toUpperCase();
+
+const hasil = [];
+
+PRODUCTS.forEach(product=>{
+
+product.aliases.forEach(alias=>{
+
+if(text.includes(alias)){
+
+let qty = 1;
+
+// cari angka setelah alias
+let regex = new RegExp(alias + "[\\s\\S]{0,80}?(\\d+)");
+
+let match = text.match(regex);
+
+if(match){
+
+qty = Number(match[1]);
+
+}
+
+hasil.push({
+
+nama:product.displayName,
+
+qty:qty
+
+});
+
+}
+
+});
+
+});
+
+return hasil;
 
 }
