f27da32d3e2474b844d72325e1fa2d0426621621
evoraparfum-stack <farhansssyah@gmail.com>
2026-07-09 00:00:14 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const PRODUCTS = [
+
+{
+displayName:"CAVENTUS / GLITCH",
+aliases:["GLITCH","CAVENTUS"]
+},
+
+{
+displayName:"CALLBLUE / INVICTUS",
+aliases:["CALLBLUE","CALBLUE","INVICTUS"]
+},
+
+{
+displayName:"ICARUS",
+aliases:["ICARUS"]
+},
+
+{
+displayName:"HAWAS ICE",
+aliases:["HAWAS ICE"]
+}
+
+];
